c66e1cf8c14a2249ccefd8f53adacf3f1393e3a2
2152001 <tokaabdelgawad60@gmail.com>
2023-11-16 18:38:00 +0200

    toka

--- Front/odc/src/main.jsx
+++ Front/odc/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './input.css'
 import Categories from './pages/categories/Categories.jsx'
+import Courses from './pages/courses/courses.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Categories />
+    <Courses />
   </React.StrictMode>,
 )

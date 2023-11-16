c4e883b73aad9db6a8931a50b6c1f4276531eae7
rawanmohameedd <nrawana12345@gmail.com>
2023-11-16 18:35:56 +0200

    sections

--- Front/odc/src/main.jsx
+++ Front/odc/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './input.css'
 import Categories from './pages/categories/Categories.jsx'
+import Section from './pages/sections/Sections.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Categories />
+    <Section />
   </React.StrictMode>,
 )
